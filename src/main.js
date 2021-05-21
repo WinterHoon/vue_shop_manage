4d6aec6abcc9ab811c486cd7cb6a94dfe8a8c645
Winter <649397454@qq.com>
2021-05-21 19:31:42 +0800

    complete login feature

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+import './assets/CSS/global.css'
+
+import axios from 'axios'
+
+//老师的接口
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// 评论区的接口
+// axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+// 导入弹框提示组件,这个组件需要全局挂载
+Vue.prototype.$message = Message
