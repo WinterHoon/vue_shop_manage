589dc79e6a944c62f3ff2fa58c464a9beaed040f
Winter <649397454@qq.com>
2021-05-28 14:23:06 +0800

    完成添加商品页面

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/CSS/global.css'
 import axios from 'axios'
 
 import TreeTable from 'vue-table-with-tree-grid'
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
  
 //老师的接口
 // axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
@@ -23,6 +29,8 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
 
 Vue.filter('dateFormat', function(originVal) {
   const date = new Date(originVal)
